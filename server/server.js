29edd7386cbee346a3641cec6f7b410433e0a23c
tor bjurbom <bjurbom1@gmail.com>
2018-08-05 17:35:20 +0200

    added server function seperated parts and started using mongoose

---
+++ server/server.js
@@ -0,0 +1,90 @@
+var express= require('express');
+var bodyParser = require('body-parser');
+
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./modules/todo');
+var {user} = require('./modules/user');
+
+
+var app = express();
+
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) =>{
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+
+    todo.save().then((doc) =>{
+        res.send(doc);
+    }, (e) =>{
+        res.status(400).send(e);
+    })
+});
+
+
+//start the server
+app.listen(3000, () =>{
+    console.log("started on port 3000")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var newTodo = new Todo({
+//    text:'Cook dinner'
+//});
+
+//newTodo.save().then((docs) =>{
+//    console.log('saved todo', docs);
+//}, (e)=>{
+//    console.log("unable to save todo");
+//});
+/*
+var otherTodo = new Todo({
+    text:' Edit this video ',
+
+});
+
+var addUser = new User({
+    email:"bjurbom1@gmail.com"
+})
+
+addUser.save().then((docs) =>{
+
+    console.log(JSON.stringify(docs,undefined,2));
+
+}, (e) =>{
+    console.log("unable to add user", e)
+});
+
+otherTodo.save().then((docs) =>{
+    console.log(JSON.stringify(docs,undefined,2));
+}, (e)=> {
+    console.log("unable to save at todo");
+});*/
