383cd15ce618a3b3c3eb3209d676e75fa61b9b7c
tor bjurbom <bjurbom1@gmail.com>
2018-08-05 18:05:10 +0200

    tested some stuff in postman

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res) =>{
+
     var todo = new Todo({
         text: req.body.text
     });
@@ -25,13 +26,21 @@ app.post('/todos',(req,res) =>{
     })
 });
 
+app.get('/todos', (req, res) =>{
+ Todo.find().then((todos) =>{
+    res.send({todos});
+ }, (e) =>{
+    res.status(400).send(e);
+ })
+}); 
+
 
 //start the server
 app.listen(3000, () =>{
     console.log("started on port 3000")
 })
 
-
+module.exports = {app};
 
 
 
