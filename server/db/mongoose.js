8b99817241f18813326c06c406d35473c0ca3238
tor bjurbom <bjurbom1@gmail.com>
2018-08-05 19:30:34 +0200

    update so it should work

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://torbj:supermario1@ds111422.mlab.com:11422/node-database-test' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
