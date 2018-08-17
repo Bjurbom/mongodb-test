dc0f193112c4985c2e94e7f157cced07c1dff489
tor bjurbom <bjurbom1@gmail.com>
2018-08-17 19:55:33 +0200

    added heroku support

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect( 'mongodb://localhost:27017/TodoApp');
+mongoose.connect( 'mongodb://torbj:supermario1@ds111422.mlab.com:11422/node-database-test' );
 //  ||
-// 'mongodb://torbj:supermario1@ds111422.mlab.com:11422/node-database-test'
+// 
+// 'mongodb://localhost:27017/TodoApp'
 
 module.exports = {
     mongoose
